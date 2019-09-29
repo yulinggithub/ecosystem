8e5dda0b7ddab2d21ba9b243c799eee4ff36faff
yuling00 <hyl2099@163.com>
2019-09-29 23:35:33 +0800

    add DecimalCollection

---
+++ src/main/java/es/upm/miw/iwvg/ecosystem/practice/DecimalCollection.java
@@ -0,0 +1,52 @@
+package es.upm.miw.iwvg.ecosystem.practice;
+
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class DecimalCollection {
+    private List<Double> collection;
+
+    public List<Double> getCollection() {
+        return collection;
+    }
+
+    public DecimalCollection() {
+        this.collection = new ArrayList<>();
+    }
+
+    public void add(double value) {
+        this.collection.add(value);
+    }
+
+    public int size() {
+        return this.collection.size();
+    }
+
+    public double sum() {
+        this.validateIsNullOrEmpty();
+        /*
+        Version java 1.7
+        double sum = 0;
+        for (double item : this.collection) {
+            sum += item;
+        }
+        return sum;
+        */
+        // return this.collection.stream().mapToDouble(value -> value).sum();
+        return this.collection.stream().mapToDouble(Double::doubleValue).sum();
+
+    }
+
+    private void validateIsNullOrEmpty() {
+        if ((this.collection == null) || this.collection.isEmpty()) {
+            throw new ArithmeticException("Null or Empty collection");
+        }
+    }
+
+    public double higher() {
+        this.validateIsNullOrEmpty();
+        return Collections.max(this.collection);
+    }
+}
